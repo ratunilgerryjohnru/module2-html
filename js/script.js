ca00c9c93fdc75b3b636716f26506746117e36a4
ratunilgerryjohnru <ratunil.gerryjohnru@gmail.com>
2026-02-19 12:14:33 +0800

    WSAT-M4: JavaScript interactivity added

--- js/script.js
+++ js/script.js
@@ -1,11 +1,5 @@
 console.log("submitBtn")
 
-const submitBtn = document.getElementById("submitBtn");
-submitBtn.addEventListener("click",function(event){
-event.preventDefault();
-alert("Thank you! Your message has been received.");
-});
-
 const themeToggle = document.getElementById("themeToggle");
 themeToggle.addEventListener("click",function(){
 document.body.classList.toggle("dark-mode");
@@ -17,6 +11,7 @@ toggleSkills.addEventListener("click",function(){
 skillsSection.classList.toggle("hidden");
 });
 
+const submitBtn = document.getElementById("submitBtn");
 submitBtn.addEventListener("click",function(event){
 event.preventDefault();
 const name = document.getElementById("name").value;
@@ -25,5 +20,6 @@ if (name===""||email===""){
 alert("Please fill in all required fields.");
 } else{
 alert("Form submitted successfully!");
+alert("Thank you! Your message has been received.");
 }
 });
